3a7ad130dbb78ba6c4adb7aeca688048157b42da
diogo-souza <digsuk360@gmail.com>
2022-10-26 19:08:11 -0300

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Data from "./components/data/Data";
+import Cloud from "./components/cloud/Cloud";
 
 function App() {
   return (
     <>
     <Navbar />
     <Hero />
+    <Data />
+    <Cloud />
     </>
   );
 }
