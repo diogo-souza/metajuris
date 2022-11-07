63f4ccb6e1c14c73db3531e8e99889fc178ba295
diogo-souza <digsuk360@gmail.com>
2022-11-06 23:27:35 -0300

    Home screen parcially finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Data from "./components/data/Data";
 import Cloud from "./components/cloud/Cloud";
 import Footer from "./components/footer/Footer";
 import Cards from "./components/cards/Cards";
@@ -11,7 +10,6 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      <Data />
       <Cloud />
       <Cards />
       <Footer />
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,14 +4,22 @@ import './HeroStyles.css';
 const Hero = () => {
   return (
     <div className='hero'>
-        <div className='container'>
-            <div className='content'>
-                <h1>Advocacia</h1>
-                <h1 className='blue'>E</h1>
-                <h1>Consultoria</h1>
-                <button>Learn More</button>
-            </div>
+      <div className="container">
+        <div className="content">
+          <h2><span className='spanGold'>NOSSA</span>  HISTÓRIA</h2>
+          <p>O MetaJuris é um Coletivo de advogados
+            empenhados em entregar soluções inovadoras e
+            práticas à realidade de seus clientes. Com
+            profissionais com mais de 25 (vinte e cinco) anos de
+            experiência nas ciências jurídicas, o escritório
+            atende a públicos variados entregando soluções
+            jurídicas de caráter preventivo e estratégias de
+            atuação no Contencioso.</p>
+          <div>
+            <button className='btnGold'>Saiba mais</button>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
