973934aff49af4e58656045dfe6cd24582647fce
diogo-souza <digsuk360@gmail.com>
2022-11-06 23:34:46 -0300

    Endereco Screen changes

--- src/components/recovery/Recovery.js
+++ src/components/recovery/Recovery.js
@@ -8,21 +8,21 @@ const Recovery = () => {
                 <div className="content">
                     <h2><span>Estrutura e</span> Endereço</h2>
                     <p>Contamos com uma estrutura confortável e
-aconchegante no coração do Poço da Panela, na Rua
-do Chacon, nº 274, Salas 416/417. 
-<br/><br/>
-Onde dispusemos de 03 (três) salas para atendimento e
-uma Sala de Reunião. O escritório detém ainda
-servidores de última geração com sistema de backup
-diário que protege todas as informações do
-escritório, sistema de Digitalização e arquivamento
-de processos em alta velocidade e sistemas
-integrados de Automação Jurídica e
-Acompanhamento Processual, favorecendo controles
-de prazos e andamentos dos processos.</p>
-                <div>
-                    <button>Learn More</button>
-                </div>
+                        aconchegante no coração do Poço da Panela, na Rua
+                        do Chacon, nº 274, Salas 416/417.
+                        <br /><br />
+                        Onde dispusemos de 03 (três) salas para atendimento e
+                        uma Sala de Reunião. O escritório detém ainda
+                        servidores de última geração com sistema de backup
+                        diário que protege todas as informações do
+                        escritório, sistema de Digitalização e arquivamento
+                        de processos em alta velocidade e sistemas
+                        integrados de Automação Jurídica e
+                        Acompanhamento Processual, favorecendo controles
+                        de prazos e andamentos dos processos.</p>
+                    <div>
+                        <button>Saiba mais</button>
+                    </div>
                 </div>
             </div>
         </div>
