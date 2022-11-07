e0f7f418a859b4e0c846f8fe6b3c0040fd8585b8
diogo-souza <digsuk360@gmail.com>
2022-11-06 21:55:58 -0300

    Cards Screen finished

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,50 +1,104 @@
 import React from 'react'
 import './CardsStyles.css'
+import direitocivil from '../../assets/direitocivil.png'
+import direitobancario from '../../assets/direitobancario.png'
+import direitoempresarial from '../../assets/direitoempresarial.jpeg'
+import processosadministrativos from '../../assets/processosadministrativos.jpg'
+import direitotrabalhista from '../../assets/direitotrabalhista.jpg'
+
+import direitocivillogo from '../../assets/direitocivil-logo.jpg'
+import direitobancarilogo from '../../assets/direitobancario-logo.jpg'
+import direitoempresariallogo from '../../assets/direitoempresarial-logo.jpeg'
+import processosadm from '../../assets/processosadm-logo.jpg'
+import direitotrabalhistalogo from '../../assets/direitotrabalhista-logo.jpg'
 
 const Cards = () => {
-    return (
-        <>
+  return (
+    <>
+      <div className='card'>
+        <div className="container">
+          <div className="content">
+            <h2><span className='spanSpecial'>Áreas de Atuação</span> </h2>
+            <div class="line-1"></div>
 
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
-        <div class="btn">
-</div>
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
 
-<div class="first heroLol">
-  <img class="heroLol-profile-img" src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg" alt=""/>
-  <div class="heroLol-description-bk"></div>
-  <div class="heroLol-logo">
-    <img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
-  </div>
-  <div class="heroLol-description">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-  <div  class="heroLol-date">
-    <p>20.02.2019</p>
-  </div>
-  <div class="heroLol-btn">
-    <a href="https://metajuris.herokuapp.com/">Learn More</a>
-  </div>
-</div>
+            <div class="first cardFirst">
+              <img class="card-profile-img" src={direitocivil} alt="" />
+              <div class="card-description-bk"></div>
+              <div class="card-logo">
+                <img src={direitocivillogo} alt="" />
+              </div>
+              <div class="card-description">
+                <p>DIREITO CIVIL PREVENTIVO, CONSULTIVO E <p className='breakLine'>CONTENCIOSO</p></p>
+              </div>
+              <div class="card-btn">
+                <a href="https://metajuris.herokuapp.com/">Saiba mais</a>
+              </div>
+            </div>
 
-<div class="second heroLol">
-  <img class="heroLol-profile-img" src="https://i.pinimg.com/originals/9f/b1/ff/9fb1ffb05dd28039d1a06ad8e90cbd75.jpg" alt=""/>
-  <div class="heroLol-description-bk"></div>
-  <div class="heroLol-logo">
-    <img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
-  </div>
-  <div class="heroLol-description">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-  <div  class="heroLol-date">
-    <p>20.02.2019</p>
-  </div>
-  <div class="heroLol-btn">
-    <a href="https://metajuris.herokuapp.com/">Learn More</a>
-  </div>
-</div>
-        </>
-    )
+            <div class="second cardSecond">
+              <img class="card-profile-img" src={direitotrabalhista} alt="" />
+              <div class="card-description-bk"></div>
+              <div class="card-logo">
+                <img src={direitotrabalhistalogo} alt="" />
+              </div>
+              <div class="card-description">
+                <p>DIREITO TRABALHISTA CONSULTIVO E CON-<p className='breakLine'>TENCIOSO</p></p>
+              </div>
+              <div class="card-btn">
+                <a href="https://metajuris.herokuapp.com/">Saiba mais</a>
+              </div>
+            </div>
+
+            <div class="third cardThird">
+              <img class="card-profile-img" src={direitoempresarial} alt="" />
+              <div class="card-description-bk"></div>
+              <div class="card-logo">
+                <img src={direitoempresariallogo} alt="" />
+              </div>
+              <div class="card-descriptionCard3">
+                <p>DIREITO EMPRESARIAL CONSULTIVO</p>
+              </div>
+              <div class="card-btn">
+                <a href="https://metajuris.herokuapp.com/">Saiba mais</a>
+              </div>
+            </div>
+
+            <div class="fourth cardFourth">
+              <img class="card-profile-img" src={direitobancario} alt="" />
+              <div class="card-description-bk"></div>
+              <div class="card-logo">
+                <img src={direitobancarilogo} alt="" />
+              </div>
+              <div class="card-descriptionCard4">
+                <p>DIREITO BANCÁRIO</p>
+              </div>
+              <div class="card-btn">
+                <a href="https://metajuris.herokuapp.com/">Saiba mais</a>
+              </div>
+            </div>
+
+            <div class="fifth cardFifth">
+              <img class="card-profile-img" src={processosadministrativos} alt="" />
+              <div class="card-description-bk"></div>
+              <div class="card-logo">
+                <img src={processosadm} alt="" />
+              </div>
+              <div class="card-descriptionCard5">
+                <p>ACOMPANHAMENTO E ATUAÇÃO EM<p className='breakLineCard5'>PROCESSOS ADMINISTRATIVOS</p></p>
+              </div>
+              <div class="card-btn">
+                <a href="https://metajuris.herokuapp.com/">Saiba mais</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Cards
