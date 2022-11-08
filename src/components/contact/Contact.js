011daefa8d9afde36f6b67d420dd635bd7236f8a
diogo-souza <digsuk360@gmail.com>
2022-11-08 20:55:01 -0300

    whatsapp-button added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './ContactStyles.css'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 const Contact = () => {
     return (
@@ -10,19 +11,27 @@ const Contact = () => {
                         <h1><span>Contact</span> Us</h1>
                         <div>
                             <label>Name</label>
-                            <input type="text" placeholder='Enter your name'/>
+                            <input type="text" placeholder='Digite seu nome' />
                         </div>
                         <div>
                             <label>Email</label>
-                            <input type="email" placeholder='Enter your email'/>
+                            <input type="email" placeholder='Digite seu email' />
                         </div>
                         <div>
                             <label>Message</label>
-                            <textarea rows='10' placeholder='Enter your name'/>
+                            <textarea rows='10' placeholder='Como podemos te ajudar?' />
                         </div>
                         <button>Submit</button>
                     </form>
                 </div>
+                <FloatingWhatsApp
+                    phoneNumber="905366030997"
+                    accountName="Metajuris"
+                    statusMessage="Online"
+                    chatMessage="Conte-nos como podemos te ajudar?"
+                    placeholder="Digite aqui..."
+
+                />
             </div>
         </div>
     )
